d7de3afc6a1485665d4844b265fd2d5dd2434a93
Raposhian <ryan.aposhian@gmail.com>
2020-02-18 14:47:54 -0700

    finished part2

--- Part2/index.js
+++ Part2/index.js
@@ -2,3 +2,13 @@ let inputId = document.getElementById('Input');
 let inputColor = document.getElementById('Color');
 
 // console.log(inputColor, inputId);
+
+function setCard() {
+   let card = document.getElementById(inputId.value);
+      card.style.color = inputColor.value;
+   // console.log(card);
+}  
+
+function reset() {
+   location.reload();
+}
